d0b646d629f9c3f49dda243e0221231bde516047
Adrian <adrian@baehr-bonn.de>
2020-04-30 15:23:41 +0200

    Accept empty Strings as well sorted

---
+++ ttd.java
@@ -0,0 +1,10 @@
+public class ttd implements ttdInterface{
+
+    @Override
+    public boolean isWellSorted(String[] sequence) {
+        if(sequence.length == 0){
+            return true;
+        }
+        return false;
+    }
+}
