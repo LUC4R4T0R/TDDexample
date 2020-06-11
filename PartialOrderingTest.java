bf0e4aa4f14932d8ef783d7d366235514fe5dc94
Adrian <adrian@baehr-bonn.de>
2020-06-11 12:00:12 +0200

    test

---
+++ PartialOrderingTest.java
@@ -0,0 +1,80 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class PartialOrderingTest {
+    @DisplayName("Test keine Bedingung | Kein Inhalt")
+    @Test
+    void leereOrdnung(){
+        PartialOrdering p = new PartialOrdering(new String[][]{});
+        Iterator s = p.iterator();
+        assertFalse(s.hasNext());
+    }
+    @DisplayName("Test eine vorhandene Bedingung| erwartet richtige Reihenfolge der beiden Elemente")
+    @Test
+    void eineBedingung(){
+        String[][] st = new String[][]{{"eins","zwei"}};
+        PartialOrdering p = new PartialOrdering(st);
+        Iterator s = p.iterator();
+        assertTrue(s.hasNext());
+        assertEquals(st[0][0], s.next());
+        assertEquals(st[0][1], s.next());
+    }
+    @DisplayName("Test zwei vorhandene Bedingung aufeinanderfolgend| erwartet richtige Reihenfolge den Elemente")
+    @Test
+    void zweiBedingung(){
+        String[][] st = new String[][]{{"eins","zwei"},{"zwei","drei"}};
+        PartialOrdering p = new PartialOrdering(st);
+        Iterator s = p.iterator();
+        assertTrue(s.hasNext());
+        assertEquals(st[0][0], s.next());
+        assertTrue(s.hasNext());
+        assertEquals(st[0][1], s.next());
+        assertEquals(st[1][1], s.next());
+    }
+    @DisplayName("Test zwei vorhandene Bedingung unabhängig| erwartet richtige mögliche Reihenfolge den Elemente")
+    @Test
+    void zweiBedingungUnabhängig(){
+        PartialOrdering p = new PartialOrdering(new String[][]{{"eins","zwei"},{"drei","vier"}});
+        Iterator<String> s = p.iterator();
+        assertTrue(s.hasNext());
+        while (s.hasNext()){
+            String temp = s.next();
+            if (temp =="eins"){
+                assertEquals("zwei", s.next());
+            }else if(temp == "drei"){
+                assertEquals("vier", s.next());
+            }else{
+                fail("eine Regel wurde nicht eingehalten");
+            }
+        }
+    }
+    @DisplayName("Test mehrere abhängige und unabhängige Regeln")
+    @Test
+    void mehrereBedingungen(){
+        String[][] st = new String[][]{{"eins", "zwei"},
+                {"eins", "drei"},
+                {"zwei", "vier"},
+                {"drei", "vier"}};
+        PartialOrdering p = new PartialOrdering(st);
+        Iterator<String> s = p.iterator();
+        assertTrue(s.hasNext());
+        assertEquals("eins", s.next());
+        String tmp = s.next();
+        boolean zweiFirst = false;
+        try {
+            assertEquals("zwei", tmp);
+            zweiFirst = true;
+        } catch (Exception e) {
+            assertEquals("drei", tmp);
+        }
+        if(zweiFirst) {
+            assertEquals("drei", s.next());
+        }else{
+            assertEquals("drei", s.next());
+        }
+        assertEquals("vier", s.next());
+    }
+}
